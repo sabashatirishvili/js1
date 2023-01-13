745331860880d4a2d65500660ede0c343e6f9fef
Saba Shatirishvili <shatirishvilis1@gmail.com>
2023-01-13 13:39:58 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/* for (let n = 0; n < 2302; n++) {
+	if (n % 2 != 0) {
+		console.log(n);
+	}
+} */
+
+function oddNumbers (until) {
+	if (until >= 0) {
+		for (let i = 1; i < until; i++) {
+			if (n % 2 != 0) {
+				console.log(n);
+			}
+		}
+	} else if (until <= 0) {
+		for (let n = 1; n > until; n--) {
+			if (n % 2 != 0) {
+				console.log(n);
+			}
+		}
+	} else {
+	}
+}
+
+oddNumbers(0);
+
+
+/*1. odd numbers
+2. print numbers as strings
+3. prints factorial of a number */
