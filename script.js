637f25975a3ae66234a3a42e2bb018c08057dbf9
Saba Shatirishvili <shatirishvilis1@gmail.com>
2023-01-14 21:01:56 +0400

    Commit

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 "use strict";
-let oddNumInput;
-let numToTextInput;
-let factorialInput;
-let operation = prompt("Operation (Odd numbers to n, Number to text, Factorial)", "Odd numbers to n");
-while (true) {
-	switch(operation) {
-		case "Odd numbers from 1 to n":
-			oddNumInput = prompt("To");
-			break;
-		case "Number to text":
-			numToTextInput = prompt("Number:");
-			break;
-		case "Factorial": 
-		factorialInput = prompt("n:");
-		break;
+function factorial(num) {
+	for (let i = 1; i < num; i++) {
+		num*=i;
 	}
 }
+
+console.log(factorial(8));
 /*function oddNumbers(oddNumTo) {
 	for(let n = 0; n < oddNumInput, n+=2) {
 
