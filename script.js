36dec9d865f2bd6713fdb7da1b1f7b0b481ab0c1
Saba Shatirishvili <shatirishvilis1@gmail.com>
2023-01-18 19:04:22 +0400

    Commit

--- script.js
+++ script.js
@@ -14,9 +14,39 @@ function oddNum(target) {
 	let strVar = "";
 	for (let i = 1; i < target; i+=2) {
 		strVar+= " " + i;
+		if(target===0) {
+			alert(i);
+			break;
+		}
 		//alert(i);
 	} 
+	if(target < 0) {
+		for(let i = 1; i > target; i-=2) {
+			for (let i = 1; i < target; i+=2) {
+				strVar+= " " + i;
+				if(target===0) {
+					alert(i);
+					break;
+				}
+		}
+	}
 	alert(strVar);
+}
+function numToText(number) {
+	const numbers = {
+		ones: {
+			1:"One",
+			2:"Two",
+			3:"Three",
+			4:"Four",
+			5:"Five",
+			6:"Six",
+			7:"Seven",
+			8:"Eight",
+			9:"Nine"
+		}
+	}
+ } 
 }
 alert(factorial(5));
 
